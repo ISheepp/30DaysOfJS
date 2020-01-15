1fe44e3769224cb6ef383be67ceb4ff50870416b
linziyang1106 <810230854@qq.com>
2020-01-15 22:42:38 +0800

    part of Day2

---
+++ js/Day2/DataType.js
@@ -0,0 +1,18 @@
+let firstName = 'Asabeneh'      // string
+let lastName = 'Yetayeh'        // string
+let country = 'Finland'         // string
+let city = 'Helsinki'           // string
+let age = 250                   // number
+let job                         // undefined, because a value was not assigned
+
+console.log(typeof 'Asabeneh')  // string
+console.log(typeof firstName)   // string
+console.log(typeof 10)          // number
+console.log(typeof 3.14)        // number
+console.log(typeof true)        // boolean
+console.log(typeof false)
+
+//parseInt(string, radix)函数解析字符串返回整数
+let num = '10'
+let numInt = parseInt(num)
+console.log(numInt) // 10
