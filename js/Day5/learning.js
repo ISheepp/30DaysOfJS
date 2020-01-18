bdf87d53f730b76c73d1f506f817caea9f9a1b62
linziyang1106 <810230854@qq.com>
2020-01-18 20:16:00 +0800

    ready Day5

---
+++ js/Day5/learning.js
@@ -0,0 +1,14 @@
+// var js = new Array()
+// var lzy = [1,2,3,4]
+// console.log(lzy)
+var js = 'javascript'
+var uper = js.split('')
+console.log(uper)
+const arr = [
+    'Asabeneh',
+    250,
+    true,
+    { country: 'Finland', city: 'Helsinki' },
+    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
+] // arr containing different data types
+console.log(arr)
